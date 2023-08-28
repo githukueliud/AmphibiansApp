e01faa7e37a002563ce8d9d765df6f6b0661ff34
dev_eliud <githukueliud@gmail.com>
2023-08-28 18:47:48 -0500

    Connected the data classes to the ui now working on rendering the ui

--- app/src/main/java/com/example/amphibiansapp/ui/screens/AmphibiansViewModel.kt
+++ app/src/main/java/com/example/amphibiansapp/ui/screens/AmphibiansViewModel.kt
@@ -4,17 +4,57 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.amphibiansapp.AmphibiansApplication
+import com.example.amphibiansapp.data.AmphibiansAppRepository
+import com.example.amphibiansapp.model.AmphibiansPhoto
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
 
 sealed interface AmphibiansUiState{
-    data class Success(val photos: String): AmphibiansUiState
+    data class Success(val photos: List<AmphibiansPhoto>): AmphibiansUiState
     object Error: AmphibiansUiState
     object Loading: AmphibiansUiState
 }
 
 
 
-class AmphibiansViewModel: ViewModel() {
+class AmphibiansViewModel(
+    private val amphibiansAppRepository: AmphibiansAppRepository
+): ViewModel() {
     //this stores the status of the most recent request
     var amphibiansUiState: AmphibiansUiState by mutableStateOf(AmphibiansUiState.Loading)
         private set
+
+    //get the information from the Amphibians Api retrofit service
+    fun getPhotos() {
+        viewModelScope.launch {
+            amphibiansUiState = AmphibiansUiState.Loading
+            amphibiansUiState = try {
+                val result = amphibiansAppRepository.getPhotos()[0]
+                AmphibiansUiState.Success(
+                    amphibiansAppRepository.getPhotos()
+                )
+            } catch (e: IOException) {
+                AmphibiansUiState.Error
+            } catch (e: HttpException) {
+                AmphibiansUiState.Error
+            }
+        }
+    }
+
+    companion object {
+        val Factory: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                val application = (this[APPLICATION_KEY] as AmphibiansApplication)
+                val amphibiansAppRepository = application.container.amphibiansAppRepository
+                AmphibiansViewModel(amphibiansAppRepository = amphibiansAppRepository)
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/amphibiansapp/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/amphibiansapp/ui/screens/HomeScreen.kt
@@ -1,20 +1,46 @@
 package com.example.amphibiansapp.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.amphibiansapp.R
 
 
 @Composable
-fun HomeScreen(modifier: Modifier = Modifier) {}
+fun HomeScreen(
+    amphibiansUiState: AmphibiansUiState ,
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    when(amphibiansUiState) {
+        is AmphibiansUiState.Loading -> LoadingScreen(modifier = Modifier.fillMaxSize())
+        is AmphibiansUiState.Success -> SuccessScreen()
+        is AmphibiansUiState.Error -> ErrorScreen(retryAction ,modifier = Modifier.fillMaxSize())
+    }
+
+}
 
 @Composable
-fun ErrorScreen(modifier: Modifier = Modifier) {
-    Text(
-        text = stringResource(R.string.loading_failed)
+fun ErrorScreen(retryAction: () -> Unit ,modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.ic_connection_error),
+        contentDescription = ""
     )
+    Text(
+        text = stringResource(R.string.loading_failed),
+        modifier = Modifier.padding(16.dp)
+    )
+    Button(onClick = retryAction) {
+        Text(text = stringResource(R.string.retry) )
+    }
 }
 
 @Composable
@@ -26,7 +52,10 @@ fun SuccessScreen(modifier: Modifier = Modifier) {
 
 @Composable
 fun LoadingScreen(modifier: Modifier = Modifier) {
-    Text(
-        text = stringResource(R.string.loading)
+    Image(
+        modifier = Modifier.size(200.dp),
+        painter = painterResource(id = R.drawable.loading_img),
+        contentDescription = stringResource(id = R.string.loading)
     )
+
 }
